8cb1dae3b00058abdce62a2f79c861579ce85efe
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-02-16 20:37:57 +0000

    adding questions and inputs for the command line prompts

--- index.js
+++ index.js
@@ -5,30 +5,63 @@ const inquirer = require('inquirer');
 inquirer.prompt([
     {
         type: 'input',
-        name: 'name',
-        message: 'What is your name?'
+        name: 'title',
+        message: 'What is the title of your project?'
     },
     {
         type: 'input',
-        name: 'location',
-        message: 'Where are you located?'
+        name: 'description',
+        message: 'Describe your project:'
     },
     {
         type: 'input',
-        name: 'bio',
-        message: 'Please provide a short bio:'
+        name: 'contents',
+        message: 'List the contents of your project:'
     },
     {
         type: 'input',
-        name: 'linkedin',
-        message: 'What is your LinkedIn URL?'
+        name: 'installation',
+        message: 'Installation guidance:'
     },
     {
         type: 'input',
-        name: 'github',
-        message: 'What is your GitHub URL?'
+        name: 'usage',
+        message: 'Usage:'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Select License:',
+        choices: [
+            'MIT License',
+            'Apache License 2.0',
+            'GNU General Public License v3.0 (GPL-3.0)',
+            'GNU General Public License v2.0 (GPL-2.0)',
+            'BSD 2-Clause "Simplified" License',
+            'BSD 3-Clause "New" or "Revised" License',
+            'Mozilla Public License 2.0',
+            'Creative Commons Zero v1.0 Universal',
+            'The Unlicense',
+            'Eclipse Public License 2.0'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Contributing:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Tests:'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Questions:'
     }
-]).then(answers => {
+])
+.then(answers => {
     // Handle the user's responses here
     console.log(answers);
     function generateHTML(userInput) {
